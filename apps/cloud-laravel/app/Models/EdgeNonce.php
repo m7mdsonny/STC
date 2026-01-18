3c93359d62a37f0af8dbd863a15a6353b119a157
m7mdsonny <83029579+m7mdsonny@users.noreply.github.com>
2026-01-18 05:22:49 +0200

    FIX: EdgeNonce model should not extend BaseModel - BaseModel uses SoftDeletes which requires deleted_at column - edge_nonces table doesn't have deleted_at column - Change EdgeNonce to extend Model directly - Fixes SQL error: Unknown column 'edge_nonces.deleted_at'

--- apps/cloud-laravel/app/Models/EdgeNonce.php
+++ apps/cloud-laravel/app/Models/EdgeNonce.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class EdgeNonce extends BaseModel
+class EdgeNonce extends Model
 {
     protected $table = 'edge_nonces';
     
